36fee20ed764d8b56e7f76b09559bd91125002a9
owozsh <accounts@owozsh.dev>
2022-02-28 12:56:29 -0300

    fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from "next";
-import Layout from "../components/layout";
-import Today from "../views/today";
 
 const Home: NextPage = () => {
-  return (
-    <Layout>
-      <Today />
-    </Layout>
-  );
+  return <></>;
 };
 
 export default Home;
